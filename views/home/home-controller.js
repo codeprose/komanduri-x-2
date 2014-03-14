e9e842ba342b715e800c3e3d3baa4a2084f69b4b
Samuel Nissim <samnissim@gmail.com>
2014-03-14 16:58:33 -0500

    removed layouts not being used

--- views/home/home-controller.js
+++ views/home/home-controller.js
@@ -1,4 +1,4 @@
-﻿$(function () {
+﻿//$(function () {
     MyApp.home = function (params) {
         alert('loading home controller...');
 
@@ -29,4 +29,4 @@
 
         return viewModel;
     };
-});
+//});
