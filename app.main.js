2a5f617f2d19daadfaa6579a887e51ad3327ace6
Samuel Nissim <samnissim@gmail.com>
2014-03-14 17:30:32 -0500

    changed to empty layout

--- app.main.js
+++ app.main.js
@@ -7,25 +7,25 @@ $(function () {
         document.addEventListener("deviceready", function () { navigator.splashscreen.hide(); });
 
         MyApp.app = new DevExpress.framework.html.HtmlApplication({
-            namespace: MyApp,
-            navigationType: "navbar",
-            navigation: [
-                    {
-                        title: "Home",
-                        action: "#home",
-                        icon: "home"
-                    },
-                    {
-                        title: "My Library",
-                        action: "#library",
-                        icon: "favorites"
-                    },
-                    {
-                        title: "Settings",
-                        action: "#settings",
-                        icon: "info"
-                    }
-                ]
+            namespace: MyApp
+            ,navigationType: "empty"
+//            ,navigation: [
+//                    {
+//                        title: "Home",
+//                        action: "#home",
+//                        icon: "home"
+//                    },
+//                    {
+//                        title: "My Library",
+//                        action: "#library",
+//                        icon: "favorites"
+//                    },
+//                    {
+//                        title: "Settings",
+//                        action: "#settings",
+//                        icon: "info"
+//                    }
+//                ]
         });
 
         MyApp.app.router.register(":view", { view: "home" });
