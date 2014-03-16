747d4c5b240cb50d72f0e645a3f8409630c95864
Ryan Gray <jamesryangray@gmail.com>
2014-03-15 20:04:14 -0700

    removed a lot of duplicate stuff

--- debug-pgb.js
+++
@@ -1,5 +0,0 @@
-﻿document.addEventListener('deviceready', function() {
-    document.querySelector('body').addEventListener('click', function () {
-        console.log('body clicked.');
-    });
-});
