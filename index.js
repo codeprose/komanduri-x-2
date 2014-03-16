121dac6a98b6edf3931f4e9e0c05f628ad5d2788
Ryan Gray <jamesryangray@gmail.com>
2014-03-15 19:50:27 -0700

    getting there

--- index.js
+++ index.js
@@ -10,10 +10,30 @@ $(function() {
 
     MyApp.app = new DevExpress.framework.html.HtmlApplication({
         namespace: MyApp,
-        navigationType: MyApp.config.navigationType
+        navigationType: MyApp.config.navigationType,
+        navigation: [
+            {
+                title: "Home",
+                action: "#home",
+                icon: "home"
+            },
+            {
+                title: "My Library",
+                action: "#library",
+                icon: "favorites"
+            },
+            {
+                title: "Settings",
+                action: "#settings",
+                icon: "info"
+            }
+        ]
     });
 
     MyApp.app.router.register(":view", { view: "home" });
+    MyApp.app.router.register(":view", { view: "library" });
+    MyApp.app.router.register(":view", { view: "settings" });
+    MyApp.app.router.register(":view/:id", { view: "procedure", id: null });
     MyApp.app.navigate();
 });
 
