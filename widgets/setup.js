26bc4c5e324143c18d6ed1d5c5b0046cc8299138
Ryan Gray <jamesryangray@gmail.com>
2014-03-19 08:31:23 -0700

    added external templating engine to load widgets from their own files. added toolbar to card.

--- widgets/setup.js
+++ widgets/setup.js
@@ -1,46 +1,23 @@
-﻿//$(function () {
-//    $('link[rel="cp-template"]').each(function (i, link) {
-//        var templateUri = $(link).attr('href');
-//        var templatePath = templateUri.split('/');
-//        var templateName = templatePath[templatePath.length - 1].split('.')[0];
-
-//        $.ajax({
-//            "url": templateUri,
-//            "async": false,
-//            "dataType": "html",
-//            "type": "GET",
-//            "timeout": this.timeout,
-//            "success": function (templateHtml) { 
-//                var templateScript = $('<script type="text/html" id="' + templateName + '-template' + '"></script>').append(templateHtml);
-//                $('body').append(templateScript);            
-//            },
-//            "error": function(exception) {
-//                if (this.useDefaultErrorTemplate)
-//                    templateHtml = this.defaultErrorTemplateHtml.replace('{STATUSCODE}', exception.status);
-//            }.bind(this)
-//        });
-//    });
-//});
-
-(function ($) {
+﻿(function ($) {
     window.codeprose = $.extend(true, window.codeprose, {
-        widget: function (widgetName) {
+        widget: function (widgetName, widgetLocation) {
+
             return {
-                init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+                init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
                     // This will be called when the binding is first applied to an element
                     // Set up any initial state, event handlers, etc. here
 
                     try {
                         var widgetBindings = allBindings()[widgetName];
-                        var bindableTemplate = $('<div />', { "data-bind": "template: { name: '" + widgetName + "-template', data: " + JSON.stringify(widgetBindings) + " }" });
+                        var bindableTemplate = $('<div />', { "data-bind": "template: { name: '" + widgetName + "', templateUrl: '" + widgetLocation + "', data: " + JSON.stringify(widgetBindings) + " }" });
                         $(element).html(bindableTemplate);
-                    } catch(e) {
+                    } catch (e) {
                         var error = new Error('Widget binding exception.');
                         error.OriginalException = e;
                         console.log(error);
                     }
                 },
-                update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+                update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
                     // This will be called once when the binding is first applied to an element,
                     // and again whenever the associated observable changes value.
                     // Update the DOM element based on the supplied values here.
@@ -49,29 +26,14 @@
         }
     });
 
-    $(function () {
+    $(document).ready(function () {
         $('link[rel="cp-template"]').each(function (i, link) {
             var templateUri = $(link).attr('href');
             var templatePath = templateUri.split('/');
+            var templateLocation = templateUri.substring(0, templateUri.lastIndexOf('/'));
             var templateName = templatePath[templatePath.length - 1].split('.')[0];
 
-            ko.bindingHandlers[templateName] = codeprose.widget(templateName);
-
-////            $.ajax({
-////                "url": templateUri,
-////                "async": false,
-////                "dataType": "html",
-////                "type": "GET",
-////                "timeout": this.timeout,
-////                "success": function (templateHtml) {
-////                    var templateScript = $('<script type="text/html" id="' + templateName + '-template' + '"></script>').append(templateHtml);
-////                    $('body').append(templateScript);
-////                },
-////                "error": function (exception) {
-////                    if (this.useDefaultErrorTemplate)
-////                        templateHtml = this.defaultErrorTemplateHtml.replace('{STATUSCODE}', exception.status);
-////                } .bind(this)
-////            });
+            ko.bindingHandlers[templateName] = codeprose.widget(templateName, templateLocation);
         });
     });
 })(jQuery);
